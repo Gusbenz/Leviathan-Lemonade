ccb208fcb4e809fdc8c5813d0256791b4f9152a7
Gusbenz <ryan.casalino@gmail.com>
2016-09-15 13:59:12 -0400

    lcd update

--- nytimes.py
+++ nytimes.py
@@ -8,9 +8,6 @@ from time import sleep
 
 mylcd = I2C_LCD_driver.lcd()
 
-# for scolling text on LCD
-str_pad = " " * 16
-
 # NYTimes API key
 apiKey = os.environ['NY_TIMES_API_KEY']
 
@@ -19,10 +16,11 @@ while True:
         'https://api.nytimes.com/svc/topstories/v1/world.json?api-key=' + apiKey)
     data = r.json()
     jsonObj = data['results']
+    bookend = '- Story below -'
     for item in jsonObj:
         x = 1
-        for i in range(0, len(item['title'])):
-            lcd_text = item['title'][i:(i + 16)] + '.' + ' Story below.'
+        for i in range(0, len(item['title'] + bookend)):
+            lcd_text = item['title'][i:(i + 16)] + bookend
             mylcd.lcd_display_string(lcd_text, 1)
             sleep(0.2)
             x += 1
