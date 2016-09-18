ef0466c03c4e8f0b67d32003f27fd22c35fb4c12
Gusbenz <ryan.casalino@gmail.com>
2016-09-18 15:08:44 -0400

    hours

--- nytimes.py
+++ nytimes.py
@@ -5,30 +5,41 @@ import I2C_LCD_driver
 import requests
 import json
 from time import sleep
+import datetime
 
 mylcd = I2C_LCD_driver.lcd()
 
-# NYTimes API key
+#### Date / Time ####
+currentDate = datetime.datetime.now()
+hour = currentDate.hour
+minute = currentDate.minute
+####
+
+#### NYTimes API key ####
 apiKey = os.environ['NY_TIMES_API_KEY']
+####
 
 while True:
-    mylcd.lcd_clear()
-    r = requests.get(
+    if hour % 1 == 0 and minute !== 00:
+        mylcd.lcd_clear()
+        r = requests.get(
         'https://api.nytimes.com/svc/topstories/v1/world.json?api-key=' + apiKey)
-    data = r.json()
-    jsonObj = data['results']
-    for item in jsonObj:
-        x = 1
-        for i in range(0, len(item['title'])):
-            lcd_text = item['title'][i:(i + 16)] + ':'
-            mylcd.lcd_display_string(lcd_text, 1)
-            sleep(0.4)
-            mylcd.lcd_clear()
-            x += 1
-        if x > 1:
-            for i in range(0, len(item['abstract'])):
-                lcd_text2 = item['abstract'][i:(i + 16)] + '.'
-                mylcd.lcd_display_string(lcd_text2, 2)
+        data = r.json()
+        jsonObj = data['results']
+        for item in jsonObj:
+            x = 1
+            for i in range(0, len(item['title'])):
+                lcd_text = item['title'][i:(i + 16)] + ':'
+                mylcd.lcd_display_string(lcd_text, 1)
                 sleep(0.4)
                 mylcd.lcd_clear()
-                x = 1
+                x += 1
+            if x > 1:
+                for i in range(0, len(item['abstract'])):
+                    lcd_text2 = item['abstract'][i:(i + 16)] + '.'
+                    mylcd.lcd_display_string(lcd_text2, 2)
+                    sleep(0.4)
+                    mylcd.lcd_clear()
+                    x = 1
+    else:
+        mylcd.lcd_display_string('The Hour is not 14')
