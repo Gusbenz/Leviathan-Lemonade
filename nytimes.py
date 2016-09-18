848c21bce7de804919f373c62730ffe7d15bc6f7
Gusbenz <ryan.casalino@gmail.com>
2016-09-18 14:32:49 -0400

    lcd clear

--- nytimes.py
+++ nytimes.py
@@ -17,11 +17,11 @@ while True:
         'https://api.nytimes.com/svc/topstories/v1/world.json?api-key=' + apiKey)
     data = r.json()
     jsonObj = data['results']
-    bookend = '- Story below -'
     for item in jsonObj:
         x = 1
-        for i in range(0, len(item['title'] + bookend)):
-            lcd_text = item['title'][i:(i + 16)] + bookend
+        for i in range(0, len(item['title'])):
+            lcd_text = item['title'][i:(i + 16)] + ':'
+            mylcd.lcd_clear()
             mylcd.lcd_display_string(lcd_text, 1)
             sleep(0.2)
             x += 1
@@ -30,4 +30,5 @@ while True:
                 lcd_text2 = item['abstract'][i:(i + 16)] + '.'
                 mylcd.lcd_display_string(lcd_text2, 2)
                 sleep(0.3)
+                mylcd.lcd_clear()
                 x = 1
