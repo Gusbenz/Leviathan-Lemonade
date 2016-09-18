4ffa2d49cba58d90b3250357150c8f953c4b5a25
Gusbenz <ryan.casalino@gmail.com>
2016-09-18 14:33:42 -0400

    lcd clear

--- nytimes.py
+++ nytimes.py
@@ -21,9 +21,9 @@ while True:
         x = 1
         for i in range(0, len(item['title'])):
             lcd_text = item['title'][i:(i + 16)] + ':'
-            mylcd.lcd_clear()
             mylcd.lcd_display_string(lcd_text, 1)
             sleep(0.2)
+            mylcd.lcd_clear()
             x += 1
         if x > 1:
             for i in range(0, len(item['abstract'])):
